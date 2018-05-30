b05ca0c7a01002dc57242a4d2e2106087f3c0431
joe lee <as955133@gmail.com>
2018-05-30 17:29:45 +0800

    NLog 自學第二次，有點明白

--- WebApplication_Nlog_Lab02/Controllers/HomeController.cs
+++ WebApplication_Nlog_Lab02/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,34 @@ namespace WebApplication_Nlog_Lab02.Controllers
 {
     public class HomeController : Controller
     {
+        private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         public ActionResult Index()
         {
+            logger.Trace("我是Trace");
+            logger.Debug("我是Debug");
+            logger.Info("我是Info");
+            logger.Warn("我是Warn");
+            logger.Error("我是Error");
+            logger.Fatal("我是Fatal");
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            // ViewBag.Message = "Your application description page.";
+
+            try
+            {
+                int a = 6;
+                int b = 0;
+                int result = a / b;
+            }
+            catch(Exception ex)
+            {
+                logger.Fatal(ex);
+            }
 
             return View();
         }
